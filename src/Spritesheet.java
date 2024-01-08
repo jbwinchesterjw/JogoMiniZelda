224b8583ed12d565fa931744f5c68031ed15723f
jbwinchesterjw <jbwincheaster@gmail.com>
2024-01-08 18:35:15 -0300

    Adicionando spritesheet personagem principal

--- src/Spritesheet.java
+++ src/Spritesheet.java
@@ -1,2 +1,26 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 public class Spritesheet {
+
+    public static BufferedImage spritesheet;
+    public static BufferedImage player_front;
+
+    public static BufferedImage tileWall;
+
+    public Spritesheet() {
+        try {
+            spritesheet = ImageIO.read(getClass().getResource("/res/spritesheet.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        player_front = Spritesheet.getSpritesheet(0, 11, 16, 16);
+        tileWall = Spritesheet.getSpritesheet(280, 221, 16, 16);
+    }
+
+    public static BufferedImage getSpritesheet(int x, int y, int width, int height) {
+        return spritesheet.getSubimage(x, y, width, height);
+    }
+
 }
